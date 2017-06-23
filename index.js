d9ff79153fb435fd253510121d3849589bb5e96b
Rob Gleeson <robbie.gleeson@gmail.com>
2017-06-23 07:10:53 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+const config = require('./config.js');
+const regex =  new RegExp(/^[a-zA-Z][0-9][a-zA-Z][0-9]{0,1}/);
+var error;
+
+module.exports = function eircode(eircode) {
+  if (!eircode) {
+    error = new UserException('No Eircode specified');
+  }
+
+  return validateEircode(eircode);
+}
+
+function validateEircode(eircode) {
+  eircode = eircode.replace(' ', '');
+
+  if (eircode.length > 7) {
+    error =  new Error("Eircode is too long");
+    return error.toString();
+  }
+  const routingKey = eircode.substring(0, 3);
+  const uniqueIdentifier = eircode.substring(3, 7);
+
+  if (!validRoutingKey(routingKey)) {
+    error = new Error("Invalid routing key")
+    return error.toString();
+  }
+
+  if (!validUniqueIdentifier(uniqueIdentifier)) {
+    error = new Error("Invalid unique identifier")
+    return error.toString();
+  }
+
+  return true;
+}
+
+function validRoutingKey(routingKey) {
+  return config.routingKeys.indexOf(routingKey) > -1 ? true : false;
+}
+
+function validUniqueIdentifier(uniqueIdentifier) {
+  return regex.test(uniqueIdentifier) ? true : false;
+}
